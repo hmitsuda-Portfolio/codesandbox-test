5c6649d0225d84c8ad4d8d31fc109f3239eb9d8a
hmitsuda-codesandbox <107514500+hmitsuda-codesandbox@users.noreply.github.com>
2022-06-18 11:43:16 +0900

    完成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import "./styles.css";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  createIncompleteList(inputText);
+};
+
+const deleteFromInconpleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+const createIncompleteList = (text) => {
+  const li = document.createElement("li");
+  li.setAttribute("id", "incomplete-text");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const p = document.createElement("p");
+  p.innerText = text;
+  document.getElementById("incomplete-list").appendChild(li);
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    deleteFromInconpleteList(li);
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+    const lii = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerText = text;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      document.getElementById("complete-list").removeChild(lii);
+
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+    document.getElementById("complete-list").appendChild(lii);
+    lii.appendChild(addTarget);
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+  });
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    deleteFromInconpleteList(li);
+  });
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
